d42026041c4884d05c0d92b3d16905962aa148ae
S <stgr@posteo.net>
2023-10-25 12:07:51 +0200

    remove references to robbot

--- scheduling/scheduling_dqn_train.py
+++ scheduling/scheduling_dqn_train.py
@@ -2,16 +2,11 @@
 import matplotlib.pyplot as plt
 from scheduling.dqn_runner import Runner
 
-import sys
-sys.path.append(join('/Py', 'robbot'))
-from robbot_imports.SendMessage import send
-
 
 def main():
     runner = Runner()
     runner.train()
 
-    send('Simulation complete')
     plt.show()
 
 
--- scheduling_policygradient/ddpg_train.py
+++ scheduling_policygradient/ddpg_train.py
@@ -3,16 +3,11 @@ import matplotlib.pyplot as plt
 
 from scheduling_policygradient.ddpg_runner import Runner
 
-import sys
-sys.path.append(join('/Py', 'robbot'))
-from robbot_imports.SendMessage import send
-
 
 def main():
     runner = Runner()
     runner.train()
 
-    send('Simulation complete')
     plt.show()
 
 
